a8621a59a01f56f07aa1c2ccd64440cd909e629f
Chen Zhi Wei <chinzhiweiblank@gmail.com>
2020-04-19 13:02:01 +0800

    Add linting and config databaseurl

--- app.js
+++ app.js
@@ -1,55 +1,58 @@
-var fs = require('fs');
 const mongoose = require("mongoose");
-var http = require('http');
-var https = require('https');
-const expressLayouts = require('express-ejs-layouts');
+var https = require("https");
+const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
-const passport = require('passport');
-const session = require('express-session');
-const flash = require('connect-flash');
-const httpLogger = require('./logger/httpLogger');
-const logger = require('./logger/logger');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("connect-flash");
+const httpLogger = require("./logger/httpLogger");
+const logger = require("./logger/logger");
+var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
+var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
+var credentials = { key: privateKey, cert: certificate };
+const yaml = require('js-yaml');
+const fs   = require('fs');
+const doc = yaml.safeLoad(fs.readFileSync('config/test.yml', 'utf8'));
 
-require('./models/passport')(passport);
+require("./models/passport")(passport);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // MongoDB Connection
-mongoose.connect('')
-mongoose.set('debug', true);
+mongoose.connect(
+  doc.databaseUrl
+);
+mongoose.set("debug", true);
+
 // For Express Layouts
 app.use(httpLogger);
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(flash());
 app.use(
-    session({
-      secret: 'zendesk',
-      resave: false,
-      saveUninitialized: false,
-      cookie: { secure: false }
-    })
-  );
+  session({
+    secret: "zendesk",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-  });
-  
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
-//app.listen(PORT, console.log(`Process started on port ${PORT}`));
-//var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(PORT, logger.info(`Process started on port ${PORT}`));
-//httpServer.listen(5000)
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const bcrypt = require("bcrypt");
-const SALT_WORK_FACTOR = 10;
-
 const UserSchema = mongoose.Schema({
   email: {
     type: String,
@@ -21,33 +18,5 @@ const UserSchema = mongoose.Schema({
     required: false
   }
 });
-/*
-UserSchema.pre('save', function (next) {
-  var user = this;
-
-  // Hash password only when modified
-  if (!user.isModified("password")) return next();
-
-  // Generate a salt using a factor of 10
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return next(err);
-
-    // Hashing password
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-
-      // User password becomes the hashed
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-UserSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};*/
 
 module.exports = mongoose.model('User', UserSchema);
